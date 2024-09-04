27e1eaebe903ed88fb000afbf184142e893d3141
adewale-codes <adewaleabiola78@gmail.com>
2024-09-04 23:04:27 +0100

    started hero section

---
+++ app/components/SearchBar.tsx
@@ -0,0 +1,38 @@
+"use client";
+import React, { useState } from "react";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
+
+const SearchBar: React.FC = () => {
+  const [searchTerm, setSearchTerm] = useState<string>("");
+
+  const handleSearch = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log("Search Term:", searchTerm);
+  };
+
+  return (
+    <div className="flex justify-center">
+      <form
+        onSubmit={handleSearch}
+        className="flex justify-center items-center md:w-3/4 border-2 border-blue-500 rounded-lg overflow-hidden"
+      >
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className="p-4 pl-4 flex-grow outline-none text-gray-600"
+          placeholder="Search... 83 100+ products"
+        />
+        <button
+          type="submit"
+          className="bg-blue-500 text-white flex items-center justify-center px-6 py-3"
+        >
+          <MagnifyingGlassIcon className="w-5 h-5 stroke-current stroke-2" />
+          <span className="ml-2 font-bold text-xl">Search</span>
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchBar;
