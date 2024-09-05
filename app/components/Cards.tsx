3a141948c8f365e8ae74f63d0222619c2772f476
craigmindset <irediacraig@gmail.com>
2024-09-05 14:41:47 +0100

    added hover effect to card

--- app/components/Cards.tsx
+++ app/components/Cards.tsx
@@ -5,21 +5,23 @@ const Cards = () => {
   return (
     <div className='px-5 md:px-24 pt-5 pb-20'>
         <div className='flex gap-16 flex-col md:flex-row'>
-            <div className=' p-6 h-64 gap-2 flex flex-col w-80 rounded-md shadow-xl items-center justify-center'>
-                <div>
-                    <Image src={"images/one.svg"} alt='drug' width={50} height={50} />
-                </div>
-                <div className='font-bold text-xl md:text-2xl'>Generics</div>
-                <div className='text-center'>76 700+ finished dosage formulation products</div>
-            </div>
-            <div className=' p-6 h-64 gap-2 flex flex-col w-80 rounded-md shadow-xl items-center justify-center'>
+           <div className='p-6 h-64 gap-2 flex flex-col w-80 rounded-md shadow-2xl items-center justify-center hover:border-b-2 border-blue-300 hover:bg-neutral-100 hover:shadow-xl transition duration-300 ease-in-out'>
+             <div>
+               <Image src={"images/one.svg"} alt='drug' width={50} height={50} />
+             </div>
+             <div className='font-bold text-xl md:text-2xl'>Generics</div>
+             <div className='text-center'>76 700+ finished dosage formulation products</div>
+           </div>
+
+            <div className=' p-6 h-64 gap-2 flex flex-col w-80 rounded-md shadow-2xl items-center justify-center hover:border-b-2 border-blue-300 hover:bg-neutral-100 hover:shadow-xl transition duration-300 ease-in-out'>
                 <div>
                     <Image src={"images/two.svg"} alt='drug' width={50} height={50} />
                 </div>
                 <div className='font-bold text-xl md:text-2xl'>Originals</div>
                 <div className='text-center'>20+ products</div>
             </div>
-            <div className=' p-6 h-64 gap-2 flex flex-col w-80 rounded-md shadow-xl items-center justify-center'>
+
+            <div className=' p-6 h-64 gap-2 flex flex-col w-80 rounded-md shadow-xl items-center justify-center hover:border-b-2 border-blue-300 hover:bg-neutral-100 hover:shadow-xl transition duration-300 ease-in-out'>
                 <div>
                     <Image src={"images/three.svg"} alt='drug' width={50} height={50} />
                 </div>
